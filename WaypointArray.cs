9670b885ff39aae9671a46893d6166234ad08b8a
MuhammadZahid25925501 <25925501@stu.mmu.ac.uk>
2026-03-25 22:49:27 +0000

    Class WaypointArray(display, add, search, no of waypoints):
    Added a Constructor for creation of a waypint array.
    
    A property for accessing the waypoints array(if needed).
    
    Added a method to add a new waypoint.
    
    Added a display method to display all the waypoints using the (.tostring method) from Waypoint Class.
    
    A display method for total number of waypoints.
    
    Search method for finding a specific waypoint via name.

--- WaypointArray.cs
+++ WaypointArray.cs
@@ -3,33 +3,50 @@
     internal class WaypointArray //class to hold an array of Waypoint objects similar to Stack and Queue classes we have seen before, but with some specific methods to add and display waypoints in the array
     {
         private Waypoint[] waypoints;
-        private int numberOfWayPoints = 0;
-
+        private int WayPointsIndex = 0;
         public WaypointArray(int size)
         {
             waypoints = new Waypoint[size]; // create an array of Waypoint objects with the specified size
-            numberOfWayPoints = 0;
+            WayPointsIndex = 0;
         }
-
+        public Waypoint[] Waypoints { get { return waypoints; } } // property to access the waypoints array
         public void addWayPoint(Waypoint waypoint)
         {
-            if (numberOfWayPoints < waypoints.Length)
+            if (WayPointsIndex < waypoints.Length)  // check if array is full
             {
-                waypoints[numberOfWayPoints] = waypoint;
-                numberOfWayPoints++;
+                waypoints[WayPointsIndex] = waypoint;
+                WayPointsIndex++;
             }
             else
             {
-                Console.WriteLine("Waypoint array is full, cannot add more waypoints.");
+               Console.WriteLine("Waypoint array is full, cannot add more waypoints.");// give a array full message
             }
         }
-
-        public void displayAllWayPoints()
+        public void displayAllWayPoints() // display all waypoints in the array
         {
-            for (int i = 0; i < numberOfWayPoints; i++)
+            for (int i = 0; i < WayPointsIndex; i++)
             {
                 Console.WriteLine(waypoints[i].toString());
             }
+        }
+        public void getNumberOfWayPoints() // display the number of waypoints currently in the array
+        {
+            Console.WriteLine("number of Waypoints: " + WayPointsIndex);
+        }
+        public Waypoint SearchWaypoint()
+        {
+            Console.WriteLine("Enter Waypoint name:");
+            string name = Console.ReadLine();
+            for (int i = 0; i < WayPointsIndex; i++)
+            {
+                if (waypoints[i].Name.ToLower() == name.ToLower())
+                {
+                    Console.WriteLine(waypoints[i].toString());
+                    return waypoints[i];
+                }
+            }
+            Console.WriteLine("Waypoint not found.");
+            return null;
         }
     }
 }
