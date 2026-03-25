a923ea6e35ed2e2904ce2893b487f56d8dfdfe1a
MuhammadZahid25925501 <25925501@stu.mmu.ac.uk>
2026-03-25 22:34:00 +0000

    Created a Construtor for the waypoint object to store all the feature values. Changed altitude from string to int. Properties for added for each feature of the waypoint (get set method). Added a toString method for a single waypoint giving proper formated information display.

--- Waypoint.cs
+++ Waypoint.cs
@@ -6,10 +6,10 @@
         private string code;
         private string latitude;
         private string longitude;
-        private string altitude;
+        private int altitude;
         private string description;
-
-        public Waypoint(string name, string code, string latitude, string longitude, string altitude, string description)
+        // constructor for a waypoint object with all features
+        public Waypoint(string name, string code, string latitude, string longitude, int altitude, string description)
         {
             this.name = name;
             this.code = code;
@@ -18,10 +18,42 @@
             this.altitude = altitude;
             this.description = description;
         }
+        // properties for each feature of a waypoint
+        public string Name
+        {
+            get { return name; }
+            set { name = value; } 
+        }
+        public string Code
+        {
+            get { return code; }
+            set { code = value; }
+        }
+        public string Latitude
+        {
+            get { return latitude; }
+            set { latitude = value; }
+        }
+        public string Longitude
+        {
+            get { return longitude; }
+            set { longitude = value; }
+        }
+        public int Altitude
+        {
+            get { return altitude; }
+            set { altitude = value; }
+        }
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
 
-        //public string toString()
-        //{
-        //    return "WayPoint: " + name + ",Code:" + code + ",Position:[" + latitude + "," + longitude + "],Elevation:" + altitude + "m\n" + description;
-        //}
+        // display a single waypoint
+        public string toString()
+        {
+            return "WayPoint: " + name + ", Code:" + code + ", Position: [ " + latitude + " , " + longitude + " ], Elevation:" + altitude + "m\nDiscription:\n" + description + "\n";
+        }
     }
 }
