e4a8ee0ce0d94b8c293fdae8b9e61f4ac6604558
MuhammadZahid25925501 <25925501@stu.mmu.ac.uk>
2026-03-25 23:00:16 +0000

    Class Route
    Created a linklist for waypoints in a route(linklist renamed to waypoints).
    
    Added a constructor for creating a route and naming it.
    
    Added properties for accessing RouteName and number of waypoints in a route.
    
    Method for adding a waypoint object at the end of the linklist through Waypoint Class.

--- Route.cs
+++ Route.cs
@@ -2,11 +2,32 @@
 {
     internal class Route
     {
-        private LinkedList<Waypoint> routes = new LinkedList<Waypoint>();
-        private int length = 0;
+        private LinkedList<Waypoint> waypoints;
+        private int noOfWaypoints = 0;
+        private string routeName;
 
         //constructor to create an empty route
+        public Route(string routename)
+        {
+            waypoints = new LinkedList<Waypoint>();
+            noOfWaypoints = 0;
+            routeName = routename;
+        }
+        public string RouteName
+        {
+            get { return routeName; }
+            set { routeName = value; }
+        }
+        public int NoOfWaypoints
+        {
+            get { return noOfWaypoints; }
+        }
         //add a waypoint to the end of the route
+        public void addWayPoint(Waypoint waypoint)
+        {
+            waypoints.AddLast(waypoint);
+            noOfWaypoints++;
+        }
         //
         //display the route - display each waypoint in the route in order
     }
