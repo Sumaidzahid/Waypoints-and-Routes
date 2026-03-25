df996bbc4673f8226d08846e0fad6812d6861e36
MuhammadZahid25925501 <25925501@stu.mmu.ac.uk>
2026-03-25 23:21:14 +0000

    Created an waypointarray of size 5000.
    used the intial starter code method used that reads csv file to add all waypoint features to the waypoint Object and store it in the newlly created waypoint array.
    
    Refactored program.cs to add a menu Display for selection of options for displaying wapoints, routes, searching etc. this is done for easy user access and managment ease. Future development will be done here(in complete).
    
    Commented out the orignal displayWayPoint void function and added personal version accessed from WaypointArray Class.

--- Program.cs
+++ Program.cs
@@ -7,15 +7,47 @@ namespace StarterCode_WayPoints
         //directory containing file
         static string FILE_PATH = "C:\\MMU\\Algorithms and data Structures\\WAY_POINT\\"; //set path to solution directory
         static string fileName = "UK_waypoints.csv"; //file to read
-
+        static WaypointArray waypointarray = new WaypointArray(5000);
         static void Main(string[] args)
         {
             readDisplayFileWayPoints(FILE_PATH + fileName);
             //To Do : Add your test code here to show that each criteria you attempted is working
-            //2. Display all waypoints in array
+            //2. Display all waypoints in array (DONE)
             //3. Create a route and display a route
             //4. insert a waypoint in a specific place route - display edited route
             //5. remove a waypoint - display edited route
+            do             
+            {
+                Console.WriteLine("\nPlease select an option (1,2,3,4,5,6):\n");
+                Console.WriteLine("1. Display all Waypoints.");
+                Console.WriteLine("2. Total number of Waypoints.");
+                Console.WriteLine("3. Select a Waypoint.");
+                Console.WriteLine("4. Create a Route.");
+                Console.WriteLine("5. Search a Route.");
+                Console.WriteLine("6. Exit\n");
+
+                int Choice = int.Parse(Console.ReadLine());
+
+                if (Choice == 1) { waypointarray.displayAllWayPoints(); }
+                else if (Choice == 2) { waypointarray.getNumberOfWayPoints(); }
+                else if (Choice == 3) { waypointarray.SearchWaypoint(); }
+                else if (Choice == 4)
+                {
+                    Console.WriteLine("Enter Route Name:");
+                    Route route = new Route(Console.ReadLine());
+                    route.addWayPoint(waypointarray.SearchWaypoint());
+                }
+                else if (Choice == 5) { 
+                    Console.WriteLine("Route Name:");
+                    string nameofroute = Console.ReadLine();
+                }
+                else if (Choice == 6)
+                {
+                    Console.WriteLine("Exiting Program...");
+                    break;
+                }
+                else { Console.WriteLine("Invalid Choice"); }
+            } while (true);
         }
 
         static void readDisplayFileWayPoints(string fileName)
@@ -38,11 +70,11 @@ namespace StarterCode_WayPoints
                     int elevation = convertElevationToMeters(featuresInLine[5]); //some in ft, some in m - convert to meters
                     string description = buildDescription(featuresInLine);
 
-                    displayWayPoint(name, code, latitude, longitude, elevation, description);
+                    waypointarray.addWayPoint(new Waypoint(name, code, latitude, longitude, elevation, description));
                 }
             }
         }
-
+/*
         static void displayWayPoint(string name, string code, string latitude, string longitude, int elevation, string description)
         {
             Console.Write("WayPoint: " + name + ",Code:" + code);
@@ -50,8 +82,7 @@ namespace StarterCode_WayPoints
             Console.WriteLine(",Elevation:" + elevation + "m");
             Console.WriteLine(description);
         }
-
-
+*/
         //Description starts at feature 11 but it may contain commas
         //so we need to concat all remaining strings on the line to be the description
         static string buildDescription(string[] featuresInLine)
