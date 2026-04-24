c494dda6461da6824b45ba8f626674a64af777c0
MuhammadZahid25925501 <25925501@stu.mmu.ac.uk>
2026-04-24 21:55:44 +0100

    Expanded WaypointArray class functionality
    - Renamed addWayPoint(Waypoint) to getFileWaypoints(Waypoint) for seperate handeling of waypoints from .csv file.
    - Added addWayPoint() for entry via console
    - Improved input validation and error handeling.
    - Updated SearchWaypoint to return nullable Waypoint and handle null input.
    - Minor formatting and comment improvements for clarity.

--- WaypointArray.cs
+++ WaypointArray.cs
@@ -1,6 +1,7 @@
 ﻿namespace StarterCode_WayPoints
 {
-    internal class WaypointArray //class to hold an array of Waypoint objects similar to Stack and Queue classes we have seen before, but with some specific methods to add and display waypoints in the array
+    //class to hold an array of Waypoint objects similar to Stack and Queue
+    internal class WaypointArray 
     {
         private Waypoint[] waypoints;
         private int WayPointsIndex = 0;
@@ -10,21 +11,47 @@
             WayPointsIndex = 0;
         }
         public Waypoint[] Waypoints { get { return waypoints; } } // property to access the waypoints array
-        public void addWayPoint(Waypoint waypoint)
+        public void getFileWaypoints(Waypoint fileWaypoints) 
         {
-            if (WayPointsIndex < waypoints.Length)  // check if array is full
+            if (WayPointsIndex < waypoints.Length) // check if array is full
             {
-                waypoints[WayPointsIndex] = waypoint;
-                WayPointsIndex++;
+                waypoints[WayPointsIndex] = fileWaypoints; 
+                WayPointsIndex++; 
             }
-            else
-            {
-               Console.WriteLine("Waypoint array is full, cannot add more waypoints.");// give a array full message
+            else {Console.WriteLine("Waypoint array is full");} 
+        }
+        // add a waypoint to the array
+        public void addWayPoint() 
+        { 
+            if (WayPointsIndex < waypoints.Length) // check if array is full
+            { 
+                Console.WriteLine("Enter: Waypoint Name, Code, Latitude, Longitude, Elevation and Description"); 
+                Console.WriteLine("Example: Waypoint1, WP1, 40.7128 N, 74.0060 W, 10, This is a waypoint"); 
+                Console.WriteLine("specified format:"); 
+                Console.WriteLine("(separated by commas, new elevation in meters)"); 
+                string? input = Console.ReadLine();
+                if (input == null) {Console.WriteLine("Invalid input"); return; }
+                string[] features = input.Split(',');
+                string name = features[0]; 
+                string code = features[1]; 
+                string latitude = features[2]; 
+                string longitude = features[3];
+                if (!int.TryParse(features[4], out int altitude)) { Console.WriteLine("Invalid input"); return; }
+                //int altitude = int.Parse(features[4]);
+                string description = features[5]; 
+
+                Waypoint newWaypoint = new Waypoint(name, code, latitude, longitude, altitude, description); 
+                waypoints[WayPointsIndex] = newWaypoint; 
+                WayPointsIndex++; 
+            } 
+            else 
+            { 
+                Console.WriteLine("Waypoint array is full, cannot add more waypoints.");// give a array full message
             }
         }
         public void displayAllWayPoints() // display all waypoints in the array
         {
-            for (int i = 0; i < WayPointsIndex; i++)
+            for (int i=0;i<WayPointsIndex;i++)
             {
                 Console.WriteLine(waypoints[i].toString());
             }
@@ -33,10 +60,11 @@
         {
             Console.WriteLine("number of Waypoints: " + WayPointsIndex);
         }
-        public Waypoint SearchWaypoint()
+        public Waypoint? SearchWaypoint()
         {
             Console.WriteLine("Enter Waypoint name:");
-            string name = Console.ReadLine();
+            string? name = Console.ReadLine();
+            if (name == null) return null;
             for (int i = 0; i < WayPointsIndex; i++)
             {
                 if (waypoints[i].Name.ToLower() == name.ToLower())
